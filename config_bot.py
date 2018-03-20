0ad5cf92da610fcf2f52c147262ec2ff2ae96ff1
potterv <gonchar.vladimir@hotmail.com>
2018-03-20 09:15:40 +0300

    active botsevbot

--- config_bot.py
+++ config_bot.py
@@ -6,6 +6,6 @@ Created on 21 февр. 2018 г.
 @author: potterjdev
 '''
 #bot @boysevbot
-#TOKEN='528556630:AAHHgQ6-KBmWRQ_8usGDu-n4Uiyol-WqRoM'
+TOKEN='528556630:AAHHgQ6-KBmWRQ_8usGDu-n4Uiyol-WqRoM'
 # bot @sev_3_5_km_bot:
-TOKEN='512562331:AAHeyxOHxhfdWw14E7r1pTbtltI5AxYdRl4'
+#TOKEN='512562331:AAHeyxOHxhfdWw14E7r1pTbtltI5AxYdRl4'
