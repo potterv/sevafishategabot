dd52725bddc6507cf99b612afa60a10eff6ed90a
potterv <gonchar.vladimir@hotmail.com>
2018-03-20 09:10:02 +0300

    update config and  add method cinema_read

--- config_bot.py
+++ config_bot.py
@@ -5,4 +5,7 @@ Created on 21 февр. 2018 г.
 
 @author: potterjdev
 '''
-TOKEN='528556630:AAHHgQ6-KBmWRQ_8usGDu-n4Uiyol-WqRoM'
+#bot @boysevbot
+#TOKEN='528556630:AAHHgQ6-KBmWRQ_8usGDu-n4Uiyol-WqRoM'
+# bot @sev_3_5_km_bot:
+TOKEN='512562331:AAHeyxOHxhfdWw14E7r1pTbtltI5AxYdRl4'
--- parssevafisha.py
+++ parssevafisha.py
@@ -10,7 +10,7 @@
 #-------------------------------------------------------------------------------
 
 import bs4, requests
-
+import operator
 def cinemaRead():
     url_cinema='https://afisha.sevastopol.press/movie'
     s=requests.get(url_cinema)
@@ -21,7 +21,7 @@ def cinemaRead():
         cinema_list=b.select('.col-2-3 .row')[1]
         for cinema in cinema_list.select('.col-1-2'):
             films.append('***************\n')
-            #print('+++')
+
             nameFilm=cinema.select('h2')[0].getText()
             films.append(''.join([nameFilm,'\n']))
             janrFilm=cinema.select('.genre')[0].getText()
@@ -31,16 +31,76 @@ def cinemaRead():
             seances=cinema.select('.seance td')
             for seance in seances:
                 films.append(''.join([str(seance.getText()),'\n']))
-                
+
                 #print(''.join(['', str(seance.getText())]))
             #print('')
 #         print(films)
+
         for film in films:
             move=''.join([move,film])
         return move
-        
+
     except:
         print('not correct match css')
         #return films
 
-    
+def cinema_Read():
+    url_cinema='https://afisha.sevastopol.press/movie'
+    s=requests.get(url_cinema)
+    b=bs4.BeautifulSoup(s.text, "html.parser")
+    CinemaKys=[]
+    CinemaValues=[]
+    listSeance=[]
+    listKinoteatr=[]
+    grafSeance={}
+    films={}
+    move=''
+    i=0
+    try:
+        cinema_list=b.select('.col-2-3 .row')[1]
+        for cinema in cinema_list.select('.col-1-2'):
+
+            nameCinema=cinema.select('h2')[0].getText()
+            CinemaKys.append(nameCinema)
+            #nmaeCinemaKys.append(''.join([nameFilm,'\n']))
+            janrCinema=cinema.select('.genre')[0].getText()
+            CinemaValues.append(janrCinema)
+
+            seances=cinema.select('.seance td')
+            #print(seances[0].getText())
+            #films[nameCinema]=janrCinema
+            #films1=sorted(films.items,kye=operator.itemgetter(0))
+
+            #print(len(seances))
+            listKinoteatr.clear()
+            listSeance.clear()
+            i=0
+            for count in range(len(seances)):
+              if count%2==0:
+                    listKinoteatr.append(seances[count].getText())
+              else:
+                listSeance.append(seances[count].getText())
+            for kinoteatr in  listKinoteatr:
+                grafSeance[kinoteatr]=listSeance[i].split(' ')
+                i+=i
+
+            #print(listKinoteatr)
+            #print(listSeance)
+            #grafSeance[listKinoteatr]=listSeance
+            #print(grafSeance)
+            films[nameCinema]=[janrCinema,grafSeance]
+
+                #print(''.join(['', str(seance.getText())]))
+            #print('')
+        print(films)
+
+ #       for film in films:
+ #           move=''.join([move,film])
+        return films
+
+    except:
+        print('not correct match css')
+        #return films
+
+if __name__ =='__main__':
+    cinema_Read()
